b8f71c7cbb46354fdceaa8a2d20617d3bf22079e
kimyeongung <kimyeongung92@gmail.com>
2023-02-11 18:58:03 +0900

    add Get Mapping : Mono

--- src/main/java/com/example/webflux/WebfluxApplication.java
+++ src/main/java/com/example/webflux/WebfluxApplication.java
@@ -1,10 +1,30 @@
 package com.example.webflux;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
+@Slf4j
 public class WebfluxApplication {
+
+    @GetMapping("/")
+    Mono<String> hello(){
+        log.info("pos1");
+        String msg = generateHello();
+        Mono<String> m = Mono.just(msg).doOnNext(log::info).log();
+        log.info("pos2");
+        return m;
+    }
+
+    private String generateHello() {
+        log.info("generateHello()");
+        return "Hello Webflux";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebfluxApplication.class, args);
