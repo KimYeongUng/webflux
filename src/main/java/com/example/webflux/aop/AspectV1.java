6c554863bf352049bb4dfcba9618fc7024f89cb9
kimyeongung <kimyeongung92@gmail.com>
2023-02-16 23:55:37 +0900

    GetMapping aop joinPoint 적용

--- src/main/java/com/example/webflux/aop/AspectV1.java
+++ src/main/java/com/example/webflux/aop/AspectV1.java
@@ -1,17 +1,41 @@
 package com.example.webflux.aop;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
+@Component
 public class AspectV1 {
 
-    public void before(){
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    public void getMapping(){
 
     }
 
-    public void after(){
+    @Before("getMapping()")
+    public void before(JoinPoint point){
+        log.info("[Log Before],{}",point.getSignature().getName());
+    }
 
+    @AfterReturning(pointcut = "getMapping()",returning = "result")
+    public void after(JoinPoint point,Object result){
+        log.info("[Log After] {}",point.getSignature().getName());
+    }
+
+    @Around("getMapping()")
+    public Object around(ProceedingJoinPoint point)throws Throwable{
+        log.info("Around Logging");
+        try {
+            log.info("END");
+            return point.proceed();
+        } catch (Exception e){
+            log.info("ERROR");
+            log.error(e.getMessage());
+            return null;
+        }
     }
 }
